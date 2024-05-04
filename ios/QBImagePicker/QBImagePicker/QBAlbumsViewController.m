a879364044abbee60587f83cb31f4ac407bc6c3e
Mark <remarkablemark@users.noreply.github.com>
2024-05-04 19:27:46 -0400

    fix(ios): replace UIGraphicsBeginImageContext in QBAlbumsViewController.m
    Fixes #2054

--- ios/QBImagePicker/QBImagePicker/QBAlbumsViewController.m
+++ ios/QBImagePicker/QBImagePicker/QBAlbumsViewController.m
@@ -209,49 +209,47 @@ static bool isDarkMode() {
 
 - (UIImage *)placeholderImageWithSize:(CGSize)size
 {
-    UIGraphicsBeginImageContext(size);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    
-    UIColor *backgroundColor = [UIColor colorWithRed:(239.0 / 255.0) green:(239.0 / 255.0) blue:(244.0 / 255.0) alpha:1.0];
-    UIColor *iconColor = [UIColor colorWithRed:(179.0 / 255.0) green:(179.0 / 255.0) blue:(182.0 / 255.0) alpha:1.0];
-    
-    // Background
-    CGContextSetFillColorWithColor(context, [backgroundColor CGColor]);
-    CGContextFillRect(context, CGRectMake(0, 0, size.width, size.height));
-    
-    // Icon (back)
-    CGRect backIconRect = CGRectMake(size.width * (16.0 / 68.0),
-                                     size.height * (20.0 / 68.0),
-                                     size.width * (32.0 / 68.0),
-                                     size.height * (24.0 / 68.0));
-    
-    CGContextSetFillColorWithColor(context, [iconColor CGColor]);
-    CGContextFillRect(context, backIconRect);
-    
-    CGContextSetFillColorWithColor(context, [backgroundColor CGColor]);
-    CGContextFillRect(context, CGRectInset(backIconRect, 1.0, 1.0));
-    
-    // Icon (front)
-    CGRect frontIconRect = CGRectMake(size.width * (20.0 / 68.0),
-                                      size.height * (24.0 / 68.0),
-                                      size.width * (32.0 / 68.0),
-                                      size.height * (24.0 / 68.0));
-    
-    CGContextSetFillColorWithColor(context, [backgroundColor CGColor]);
-    CGContextFillRect(context, CGRectInset(frontIconRect, -1.0, -1.0));
-    
-    CGContextSetFillColorWithColor(context, [iconColor CGColor]);
-    CGContextFillRect(context, frontIconRect);
-    
-    CGContextSetFillColorWithColor(context, [backgroundColor CGColor]);
-    CGContextFillRect(context, CGRectInset(frontIconRect, 1.0, 1.0));
-    
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
+    UIGraphicsImageRenderer *renderer = [[UIGraphicsImageRenderer alloc] initWithSize:size];
+    UIImage *image = [renderer imageWithActions:^(UIGraphicsImageRendererContext * _Nonnull rendererContext) {
+        CGContextRef context = rendererContext.CGContext;
+
+        UIColor *backgroundColor = [UIColor colorWithRed:(239.0 / 255.0) green:(239.0 / 255.0) blue:(244.0 / 255.0) alpha:1.0];
+        UIColor *iconColor = [UIColor colorWithRed:(179.0 / 255.0) green:(179.0 / 255.0) blue:(182.0 / 255.0) alpha:1.0];
+
+        // Background
+        CGContextSetFillColorWithColor(context, [backgroundColor CGColor]);
+        CGContextFillRect(context, CGRectMake(0, 0, size.width, size.height));
+
+        // Icon (back)
+        CGRect backIconRect = CGRectMake(size.width * (16.0 / 68.0),
+                                         size.height * (20.0 / 68.0),
+                                         size.width * (32.0 / 68.0),
+                                         size.height * (24.0 / 68.0));
+
+        CGContextSetFillColorWithColor(context, [iconColor CGColor]);
+        CGContextFillRect(context, backIconRect);
+
+        CGContextSetFillColorWithColor(context, [backgroundColor CGColor]);
+        CGContextFillRect(context, CGRectInset(backIconRect, 1.0, 1.0));
+
+        // Icon (front)
+        CGRect frontIconRect = CGRectMake(size.width * (20.0 / 68.0),
+                                          size.height * (24.0 / 68.0),
+                                          size.width * (32.0 / 68.0),
+                                          size.height * (24.0 / 68.0));
+
+        CGContextSetFillColorWithColor(context, [backgroundColor CGColor]);
+        CGContextFillRect(context, CGRectInset(frontIconRect, -1.0, -1.0));
+
+        CGContextSetFillColorWithColor(context, [iconColor CGColor]);
+        CGContextFillRect(context, frontIconRect);
+
+        CGContextSetFillColorWithColor(context, [backgroundColor CGColor]);
+        CGContextFillRect(context, CGRectInset(frontIconRect, 1.0, 1.0));
+    }];
+
     return image;
 }
-
 
 #pragma mark - Checking for Selection Limit
 
